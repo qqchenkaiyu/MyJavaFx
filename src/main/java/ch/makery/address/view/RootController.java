72769cde0309eddd9abb1dff76228c00306686db
qqchenkaiyu <37261937+qqchenkaiyu@users.noreply.github.com>
2020-06-28 16:55:10 +0800

    Update RootController.java

--- src/main/java/ch/makery/address/view/RootController.java
+++ src/main/java/ch/makery/address/view/RootController.java
@@ -206,7 +206,7 @@ public class RootController extends Controller {
         }
         DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
         for (ServerConfig serverConfig : configs) {
-            String execResult = getExecResult(serverConfig,
+            String execResult = ExecShell(serverConfig,
                     "date -s " + dateTimeFormatter.print(System.currentTimeMillis()));
             DialogUtils.AlertInfomation("同步成功 服务器时间为" + getExecResult(serverConfig, "date"));
         }
